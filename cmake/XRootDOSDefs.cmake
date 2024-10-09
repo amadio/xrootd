amadio/xrootd

commit e6136c9ebaf56b5f17eda2f53ccafcbd930ac3b1

[CMake] Set RPATH that works for binaries and libraries on macOS

cmake/XRootDOSDefs.cmake
@@ -108,9 +108,9 @@ if( APPLE )
   set( MacOSX TRUE )
   set( XrdClPipelines TRUE )
   
-  if( NOT DEFINED CMAKE_MACOSX_RPATH )
-    set( CMAKE_MACOSX_RPATH 1 )
-  endif()
+  set(CMAKE_MACOSX_RPATH TRUE)
+  set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
+  set(CMAKE_INSTALL_RPATH "@loader_path/../lib")
 
   # this is here because of Apple deprecating openssl and krb5
   set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-deprecated-declarations" )

commit 7145f5c0db68215a7c09f45eecc6a1578d435582

[CMake] Set CMAKE_INSTALL_RPATH in the cache to allow overrides
Fixes: e6136c9ebaf56b5f17eda2f53ccafcbd930ac3b1
Closes: #2350

cmake/XRootDOSDefs.cmake
@@ -110,7 +110,7 @@ if( APPLE )
   
   set(CMAKE_MACOSX_RPATH TRUE)
   set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
-  set(CMAKE_INSTALL_RPATH "@loader_path/../lib")
+  set(CMAKE_INSTALL_RPATH "@loader_path/../lib" CACHE STRING "Install RPATH")
 
   # this is here because of Apple deprecating openssl and krb5
   set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-deprecated-declarations" )
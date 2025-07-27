417eb4f5e4fade871677fe2c9b3c5053e4fc087e
Brian Bockelman <bbockelman@morgridge.org>
2025-07-27 07:50:38 -0600

    Add tests for XrdClS3 client plugin

---
+++ tests/XrdClS3/DeleteTest.cc
@@ -0,0 +1,32 @@
+/****************************************************************
+ *
+ * Copyright (C) 2025, Pelican Project, Morgridge Institute for Research
+ *
+ ***************************************************************/
+
+#include "../XrdClCurlCommon/TransferTest.hh"
+
+#include <XrdCl/XrdClFileSystem.hh>
+
+class S3DeleteFixture : public TransferFixture {};
+
+TEST_F(S3DeleteFixture, Test)
+{
+    std::string fname = "/test-bucket/delete_file";
+    auto url = GetCacheURL() + fname;
+    WritePattern(url, 8, 'a', 2);
+    XrdCl::FileSystem fs(GetCacheURL());
+
+    XrdCl::StatInfo *response{nullptr};
+    auto st = fs.Stat(fname + "?authz=" + GetReadToken(), response, 10);
+    ASSERT_TRUE(st.IsOK()) << "Failed to stat new file: " << st.ToString();
+    delete response;
+
+    st = fs.Rm(fname + "?authz=" + GetWriteToken(), 10);
+    ASSERT_TRUE(st.IsOK()) << "Failed to remove file: " << st.ToString();
+
+    response = nullptr;
+    st = fs.Stat(fname + "?authz=" + GetReadToken(), response, 10);
+    ASSERT_FALSE(st.IsOK()) << "Stat of removed file should have failed";
+    ASSERT_EQ(st.errNo, kXR_NotFound);
+}
---
+++ tests/XrdClS3/ReadTest.cc
@@ -0,0 +1,100 @@
+/****************************************************************
+ *
+ * Copyright (C) 2025, Pelican Project, Morgridge Institute for Research
+ *
+ ***************************************************************/
+
+#include "../XrdClCurlCommon/TransferTest.hh"
+#include "XrdClS3/XrdClS3DownloadHandler.hh"
+#include "XrdClS3/XrdClS3Filesystem.hh"
+
+#include <gtest/gtest.h>
+
+#include <memory>
+
+class S3ReadFixture : public TransferFixture {
+public:
+    void SetUp() override {
+        TransferFixture::SetUp();
+    }
+
+protected:
+    XrdClCurl::HeaderCallout *GetReadTokenCallout() {
+        return &m_read_callout;
+    }
+
+private:
+    class ReadAuthzCallout : public XrdClCurl::HeaderCallout {
+        public:
+            ReadAuthzCallout(const S3ReadFixture &parent) :
+                m_parent(parent)
+            {}
+
+            virtual ~ReadAuthzCallout() = default;
+
+            virtual std::shared_ptr<HeaderList> GetHeaders(const std::string & /*verb*/, const std::string & /*url*/, const HeaderList & input_headers) {
+                std::shared_ptr<HeaderList> headers(new HeaderList(input_headers));
+                headers->emplace_back("Authorization", m_parent.GetReadToken());
+                return headers;
+            }
+
+        private:
+            const S3ReadFixture &m_parent;
+    };
+
+    ReadAuthzCallout m_read_callout{*this};
+};
+
+// Read a single file
+//
+// Because all reads happen in serial and linearly, this will test the prefetch
+// capabilities
+TEST_F(S3ReadFixture, SerialTest)
+{
+    auto chunk_ctr = 10;
+    auto url = GetCacheURL() + "/test-bucket/read_single_" + std::to_string(chunk_ctr);
+    ASSERT_NO_FATAL_FAILURE(WritePattern(url, chunk_ctr * 100'000, 'a', chunk_ctr * 10'000));
+}
+
+// Test for single-shot callback for downloading an entire URL into a buffer
+TEST_F(S3ReadFixture, OneShotTest)
+{
+    constexpr unsigned chunk_ctr = 10;
+    auto url = GetCacheURL() + "/test-bucket/read_singleshot";
+    ASSERT_NO_FATAL_FAILURE(WritePattern(url, chunk_ctr * 100'000, 'a', chunk_ctr * 10'000));
+
+    std::unique_ptr<SyncResponseHandler> srh(new SyncResponseHandler());
+    auto st = XrdClS3::DownloadUrl(url, GetReadTokenCallout(), srh.get(), XrdClS3::Filesystem::timeout_t{10});
+    ASSERT_TRUE(st.IsOK());
+
+    srh->Wait();
+    auto [status, object] = srh->Status();
+    ASSERT_TRUE(status != nullptr);
+    ASSERT_TRUE(status->IsOK()) << "Failed to run full-download of object: " << status->ToString();
+    ASSERT_TRUE(object != nullptr);
+    XrdCl::Buffer *buffer = nullptr;
+    object->Get(buffer);
+    ASSERT_TRUE(buffer);
+    ASSERT_EQ(buffer->GetSize(), chunk_ctr * 100'000);
+
+    off_t chunkSize = chunk_ctr * 10'000;
+    off_t expectedSize = chunk_ctr * 100'000;
+    auto sizeToRead = (static_cast<off_t>(chunkSize) >= expectedSize)
+                                        ? expectedSize
+                                        : chunkSize;
+    unsigned char curChunkByte = 'a';
+    buffer->SetCursor(0);
+    while (sizeToRead) {
+        std::string readBuffer(buffer->GetBufferAtCursor(), chunk_ctr * 10'000);
+
+        std::string correctBuffer(sizeToRead, curChunkByte);
+        ASSERT_EQ(readBuffer, correctBuffer);
+
+        expectedSize -= sizeToRead;
+        buffer->AdvanceCursor(sizeToRead);
+        sizeToRead = (static_cast<off_t>(chunkSize) >= expectedSize)
+                                            ? expectedSize
+                                            : chunkSize;
+        curChunkByte += 1;
+    }
+}

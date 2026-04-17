2092c432f3a92a72bf291209be3e39a7eb8b0230
Guilherme Amadio <guilherme@amadio.org>
2026-04-17 15:04:14 +0200

    [XrdOuc] Switch is_subdirectory() to use std::string_view
    This is to allow it to be used more efficiently in places where
    we do not have a std::string ready, but do have a char* + length,
    to avoid copying memory into a std::string before calling it.

--- src/XrdOuc/XrdOucPrivateUtils.hh
+++ src/XrdOuc/XrdOucPrivateUtils.hh
@@ -26,15 +26,15 @@
 #include "XrdOuc/XrdOucString.hh"
 
 #include <regex>
-#include <string>
+#include <string_view>
 #include <unordered_set>
 #include <vector>
 
 /**
  * Returns true if path @p subdir is a subdirectory of @p dir.
  */
-static inline bool is_subdirectory(const std::string& dir,
-                                   const std::string& subdir)
+static inline bool is_subdirectory(const std::string_view dir,
+                                   const std::string_view subdir)
 {
     if (subdir.size() < dir.size() || dir.empty())
       return false;
